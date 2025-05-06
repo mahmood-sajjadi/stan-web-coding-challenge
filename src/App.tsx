3d361729463ba9ae4bfd000243e74ee37f22eda7
Mahmood <mahmood.sajjadi@gmail.com>
2025-05-06 21:14:09 +1000

    adding structure and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import React, { useMemo } from 'react';
 import './App.css'
+import { AppRoutes } from "./routes/AppRoutes";
+import { ApiServiceContext } from './context/ApiServiceContext';
+import { apiServiceFactory } from './services/apiService';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const apiService = useMemo(() => apiServiceFactory({
+    //later we can get base url from .env file
+    baseUrl: '/api/'
+  }), []);
 
   return (
-    "test"
+    <ApiServiceContext.Provider value={apiService}>
+      <AppRoutes />
+    </ApiServiceContext.Provider>
   )
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import mockResponse from './challenge/feed/sample.json'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy) => {
+          proxy.on('proxyReq', (_, req, res) => {
+            if (req.url === '/api/series' || req.url === '/api/movies') {
+              res.writeHead(200, { 'Content-Type': 'application/json' });
+              res.end(JSON.stringify(mockResponse));
+            }
+          });
+        }
+      },
+    },
+  },
 })
